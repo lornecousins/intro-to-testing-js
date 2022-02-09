1049a10bd54de0ad689caf45fcefb3f05418e071
Lorne Cousins <lorne.cousins88@gmail.com>
2022-02-09 00:22:47 -0600

    intro_to_testing eercise complete

--- code.js
+++ code.js
@@ -1,7 +1,16 @@
-// helloWorld function 
+// helloWorld function
 function helloWorld() {
     return "Hello, World!";
 }
-function sayHello() {
-    return "Say Hello"
+//
+function sayHello(input) {
+//     if (input === 'Alex') {
+//         return "Hello, Alex!";
+//     } else if (input === 'Pat') {
+//         return "Hello, Pat!";
+//     }
+//     else {
+//         return "Hello, Jane!";
+function sayHello(input) {}
+return 'Hello, ' + input + '!';
 }
